181c923d0382f8b7bc2218f0bb4f74d69f4288d4
murayama_misato <murayama.misato@alhinc.com>
2024-09-20 16:31:31 +0900

    17時最終push

--- src/main/java/com/example/ToYokoNA/controller/form/TaskForm.java
+++ src/main/java/com/example/ToYokoNA/controller/form/TaskForm.java
@@ -1,6 +1,7 @@
 package com.example.ToYokoNA.controller.form;
 
-import jakarta.validation.constraints.NotEmpty;
+import com.example.ToYokoNA.Validation.Past;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,7 +15,7 @@ public class TaskForm {
     @NotNull
     private int id;
 
-    @NotEmpty(message = "タスクを入力してください")
+    @NotBlank(message = "タスクを入力してください")
     @Size(max = 140, message = "タスクは140文字以内で入力してください")
     private String content;
 
@@ -25,4 +26,8 @@ public class TaskForm {
 
     private Date createdDate;
     private Date updatedDate;
+    @Past
+    @NotBlank(message = "期限を設定してください")
+    private String strLimitDate;
+
 }
